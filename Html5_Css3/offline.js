b25aac38ede32eb52f3514a0ca7abc523502d8a7
jarbas coltro <jarbascltr@gmail.com>
2016-02-20 07:44:33 -0800

    offline

--- Html5_Css3/offline.js
+++ Html5_Css3/offline.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function()  {
   var status = document.getElementById("status");
 
   function updateOnlineStatus(event) {
